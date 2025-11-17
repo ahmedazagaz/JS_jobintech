69b0d72800bee267df920c7072ebfbc2a25c512b
ahmedazagaz <contactwithazagaz@gmail.com>
2025-11-17 01:44:54 +0100

    Point

--- 3-Vector/Point.js
+++ 3-Vector/Point.js
@@ -1,13 +1,12 @@
-//class Point
 class Point {
-    constructor(x = 0.0, y = 0.0) {
+    constructor(x = 0, y = 0) {
         this.x = x;
         this.y = y;
     }
 
     afficher() {
-        console.log(`Point(x=${this.x}, y=${this.y})`);
+        console.log(`Point: x=${this.x}, y=${this.y}`);
     }
 }
 
-module.exports = Point;
+export default Point;
