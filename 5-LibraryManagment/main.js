3be45d38a78ad6cc37b361782e6a77e499bf1f76
ahmedazagaz <contactwithazagaz@gmail.com>
2025-11-17 01:33:39 +0100

    main

---
+++ 5-LibraryManagment/main.js
@@ -0,0 +1,12 @@
+import booksManagement from './books.js';
+import categoriesManagement from './categories.js';
+import borrowsManagement from './borrows.js';
+
+console.log("-- Gestion des livres --");
+booksManagement();
+
+console.log("\n-- Gestion des catégories --");
+categoriesManagement();
+
+console.log("\n-- Gestion des emprunts --");
+borrowsManagement();
