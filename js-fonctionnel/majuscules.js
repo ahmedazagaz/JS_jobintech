3cd0552be31417eada3d6763ac28931a6f1a9f31
ahmedazagaz <contactwithazagaz@gmail.com>
2025-11-23 00:12:01 +0100

    MAJ

--- js-fonctionnel/majuscules.js
+++ js-fonctionnel/majuscules.js
@@ -1,7 +1,10 @@
 const texte = `jobintech`;
 
+// Transformer le texte
 const result = texte
-  .split('\n')               
-  .map(line => line.toUpperCase())
-  .filter(line => line.includes('I'));
-result.forEach(line => console.log(line)); //JOBINTECH
+  .split('\n')                    
+  .map(line => line.toUpperCase())   // Convertit chaque ligne en majuscules
+  .filter(line => line.includes('I')); // Garde seulement les lignes avec "I"
+
+// Afficher le résultat
+result.forEach(line => console.log(line));
